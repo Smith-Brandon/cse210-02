5199ae465713ee51729576a3af2f6fc69b7f4226
mckayythomas <mckayycollins@gmail.com>
2022-09-29 19:55:11 -0500

    Base foundation

---
+++ cse210-02/cse210-02/player.py
@@ -0,0 +1,28 @@
+from deck import Deck
+
+class Player:
+    '''comments'''
+    def __init__(self):
+        pass
+
+
+    '''Method comments'''
+    def start_game(self):
+        pass
+
+    '''Method comments'''
+    def get_inputs(self):
+        pass
+
+
+    '''Method comments'''
+    def do_updates(self):
+        pass
+
+
+    '''Method comments'''
+    def do_outputs(self):
+        pass
+
+
+    
