037b3402d79682473d63c7fa3cb2016d7cb9271e
Brandon Smith <smi11037@byui.edu>
2022-10-01 19:37:06 -0600

    Added basic do_update method.

--- cse210-02/cse210-02/player.py
+++ cse210-02/cse210-02/player.py
@@ -15,8 +15,26 @@ class Player:
         pass
 
 
-    '''Method comments'''
+    '''Method compares the users guess with the last number and updates score.'''
     def do_updates(self):
+        # If keep_playing is false return
+        if not self.keep_playing:
+            return
+
+        # Get correct answer
+        if(self.point < self.last_point):
+            self.answer = "l"
+        elif(self.point > self.last_point):
+            self.answer = "h"
+        
+        # Update score value
+        if(self.guess == self.answer):
+            self.score += 100
+        else:
+            self.score -= 75
+
+        # Set last_point for next round
+        self.last_point = self.point
         pass
 
 
