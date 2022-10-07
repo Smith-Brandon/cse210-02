deeb39f333270c95165e10a5d683d9b95dd305aa
Brandon Smith <smi11037@byui.edu>
2022-10-06 18:31:57 -0600

    Made changes to integrate get_guess.

--- cse210-02/cse210-02/player.py
+++ cse210-02/cse210-02/player.py
@@ -3,37 +3,44 @@ from deck import Card
 
 '''Create Player class and start the game. The player has a few responsibilities. 
 To start the game, keep score, make a card guess, and update score according to players choice. '''
+
+
 class Player:
-    
+
     def __init__(self):
         '''Create values for the player class to be initalized.'''
         self.playing = True
         self.score = 300
         self.card = Card()
         self.card1 = 0
+        self.new_card = 0
         self.guess = ""
 
-
     def start_game(self):
-            '''Starts the game by going throuhg all required methods in order to play. The order will be 
-            Show card, Draw card, Player guess, update points, and repeat until player no longer wishes
-            continue or player runs out of points.'''
-            self.card1 = self.card.draw_card()
+        '''Starts the game by going throuhg all required methods in order to play. The order will be 
+        Show card, Draw card, Player guess, update points, and repeat until player no longer wishes
+        continue or player runs out of points.'''
+        self.card1 = self.card.draw_card()
 
-            while self.playing:
-                self.get_inputs()
-                self.do_updates()
-                self.do_outputs()
+        while self.playing:
+            self.get_guess()
+            self.do_updates()
+            self.do_outputs()
+            self.continue_playing()
 
+    def get_guess(self):
+        """Asks the user if they think the next card is higher or lower
 
-    '''Method comments'''
-
-    def get_inputs(self):
-        pass
-
-    '''Method comments'''
+        Args:
+            self (Player): An instance of the player
+        """
+        point = list(self.card1.keys())[0]
+        suit = list(self.card1.values())[0]
+        print(f"The card is {point} of {suit}")
+        self.guess = input("Higher or lower? [h/l] ")
 
     def do_updates(self):
+        answer = ""
         # If keep_playing is false return
         if not self.playing:
             return
@@ -50,19 +57,19 @@ class Player:
         suit2 = list(new_card.values())[0]
 
         # Check if the same card was drawn
-        if(new_card == self.card1):
-            while(new_card == self.card1):
+        if (new_card == self.card1):
+            while (new_card == self.card1):
                 card = Card()
                 new_card = card.draw_card()
 
         # Get correct answer
-        if(point2 < point1):
+        if (point2 < point1):
             answer = "l"
-        elif(point2 > point1):
+        elif (point2 > point1):
             answer = "h"
-        
+
         # Update score value
-        if(self.guess == answer):
+        if (self.guess == answer):
             self.score += 100
         else:
             self.score -= 75
@@ -71,15 +78,27 @@ class Player:
         self.card1 = {point2: suit2}
 
         # Check for end of game
-        if(self.score == 0):
-            userInput = input("Do you want to play again: [y/n]: ")
-            if (userInput.lower == "y"):
-                self.playing = True
-            elif(userInput.lower == "n"):
-                self.playing = False
+        if (self.score == 0):
+            print("You lost!")
         pass
 
-    '''Method comments'''
+    def continue_playing(self):
+        """Asks the user if they want to continue playing
+
+        Args:
+            self (Player): An instance of the player
+        """
+        cont_game = input("Do you want to continue the game? [y/n] ")
+        if cont_game == "y":
+            self.playing = True
+        elif cont_game == "n":
+            self.playing = False
+        print("\n")
 
     def do_outputs(self):
+        point = list(self.card1.keys())[0]
+        suit = list(self.card1.values())[0]
+        print(f"Next card was: {point} of {suit}")
+
+        print(f"Your score is: {self.score}")
         pass
